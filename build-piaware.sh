63bbf437c90c6df4be6069238b41379980a7893e
abcd567a <abcd567@hotmail.com>
2023-11-23 12:57:02 -0500

    Update and rename install.sh to build-piaware.sh

--- build-piaware.sh
+++ build-piaware.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
-CLONED_FOLDER=${PWD}
-
+BUILD_FOLDER=/tmp/piaware-builder
 sudo pacman --needed -Sy fakeroot binutils git pkgconf autoconf make gcc glibc patch which
-sudo pacman --needed -Sy tcl python python-setuptools tk net-tools
+sudo pacman --needed -Sy tcl python python-setuptools python-pip tk net-tools
 
-cd ~/
+cd ${BUILD_FOLDER}
 git clone https://aur.archlinux.org/tcllib
 cd tcllib
 makepkg -si
 
-cd ~/
+cd ${BUILD_FOLDER}
 git clone https://aur.archlinux.org/tclx
 cd tclx
 makepkg -si
 
-cd ~/
+cd ${BUILD_FOLDER}
 git clone https://aur.archlinux.org/tcltls
 cd tcltls
 makepkg -si
 
-cd ~/
+cd ${BUILD_FOLDER}
 git clone https://aur.archlinux.org/tcllauncher
 cd tcllauncher
 makepkg -si
 
-cd ${CLONED_FOLDER}
+cd ${BUILD_FOLDER}/archlinux-piaware
 makepkg -si
 
 
