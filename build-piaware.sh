070d7c39176a7f7d17db81aafa2648ae24014568
abcd567a <abcd567@hotmail.com>
2023-11-30 14:22:23 -0500

    Update build-piaware.sh

--- build-piaware.sh
+++ build-piaware.sh
@@ -3,6 +3,7 @@
 echo ""
 echo -e "\e[1;32mInstalling pacckages to provide Build Tools and Dependencies \e[39m"
 echo ""
+sleep 1
 BUILD_FOLDER=${PWD}
 sudo pacman --needed -S fakeroot git gcc make binutils pkgconf glibc which
 sudo pacman --needed -S patch autoconf tcl tk openssl
@@ -12,6 +13,7 @@ sudo pacman --needed -S python-build python-installer
 echo ""
 echo -e "\e[1;32mBuilding and Installing package tcllib \e[39m"
 echo ""
+sleep 1
 cd ${BUILD_FOLDER}
 git clone https://aur.archlinux.org/tcllib
 cd tcllib
@@ -20,6 +22,7 @@ makepkg -si
 echo ""
 echo -e "\e[1;32mBuilding and Installing package tclx \e[39m"
 echo ""
+sleep 1
 cd ${BUILD_FOLDER}
 git clone https://aur.archlinux.org/tclx
 cd tclx
@@ -28,6 +31,7 @@ makepkg -si
 echo ""
 echo -e "\e[1;32mBuilding and Installing package tcltls \e[39m"
 echo ""
+sleep 1
 cd ${BUILD_FOLDER}
 git clone https://aur.archlinux.org/tcltls
 cd tcltls
@@ -36,6 +40,7 @@ makepkg -si
 echo ""
 echo -e "\e[1;32mBuilding and Installing packages tcllauncher \e[39m"
 echo ""
+sleep 1
 cd ${BUILD_FOLDER}
 git clone https://aur.archlinux.org/tcllauncher
 cd tcllauncher
@@ -46,6 +51,7 @@ echo -e "\e[1;95mInstallation of Build Tools and Dependency packages completed \
 echo ""
 echo -e "\e[1;32mNow Building and Installing package PIAWARE \e[39m"
 echo ""
+sleep 2
 cd ${BUILD_FOLDER}/archlinux-piaware
 makepkg -si
 
