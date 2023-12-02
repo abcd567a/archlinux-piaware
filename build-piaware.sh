653e98d96ccf4b839fc4414d51b512ad8596ec58
abcd567a <abcd567@hotmail.com>
2023-12-01 22:47:19 -0500

    Update build-piaware.sh

--- build-piaware.sh
+++ build-piaware.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-echo ""
-echo -e "\e[1;32mInstalling pacckages to provide Build Tools and Dependencies \e[39m"
-echo ""
-sleep 1
 BUILD_FOLDER=${PWD}
-sudo pacman --needed -S fakeroot git gcc make binutils pkgconf glibc which
-sudo pacman --needed -S patch autoconf tcl tk openssl
-sudo pacman --needed -S python python-setuptools python-pip net-tools
-sudo pacman --needed -S python-build python-installer  
-
+echo ""
+echo -e "\e[1;32mInstalling (if not already installed) following Build Tools \e[39m"
+echo -e "\e[1;32m   git fakeroot patch gcc make autoconf \e[39m"
+sleep 3
+sudo pacman --needed -S git fakeroot patch gcc make autoconf
+echo ""
+echo -e "\e[1;95mBuilding and Installing following packages \e[39m"
+echo -e "\e[1;95mwhich are NOT available in Arch Linux repo \e[39m"
+echo -e "\e[1;95m  tcllib tclx tcltls tcllauncher \e[39m"
 echo ""
 echo -e "\e[1;32mBuilding and Installing package tcllib \e[39m"
 echo ""
-sleep 1
+sleep 2
 cd ${BUILD_FOLDER}
 git clone https://aur.archlinux.org/tcllib
 cd tcllib
@@ -22,7 +22,7 @@ makepkg -si
 echo ""
 echo -e "\e[1;32mBuilding and Installing package tclx \e[39m"
 echo ""
-sleep 1
+sleep 2
 cd ${BUILD_FOLDER}
 git clone https://aur.archlinux.org/tclx
 cd tclx
@@ -31,7 +31,7 @@ makepkg -si
 echo ""
 echo -e "\e[1;32mBuilding and Installing package tcltls \e[39m"
 echo ""
-sleep 1
+sleep 2
 cd ${BUILD_FOLDER}
 git clone https://aur.archlinux.org/tcltls
 cd tcltls
@@ -40,7 +40,7 @@ makepkg -si
 echo ""
 echo -e "\e[1;32mBuilding and Installing packages tcllauncher \e[39m"
 echo ""
-sleep 1
+sleep 2
 cd ${BUILD_FOLDER}
 git clone https://aur.archlinux.org/tcllauncher
 cd tcllauncher
@@ -51,7 +51,7 @@ echo -e "\e[1;95mInstallation of Build Tools and Dependency packages completed \
 echo ""
 echo -e "\e[1;32mNow Building and Installing package PIAWARE \e[39m"
 echo ""
-sleep 2
+sleep 3
 cd ${BUILD_FOLDER}/archlinux-piaware
 makepkg -si
 
